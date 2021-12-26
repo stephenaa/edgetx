78c8157ff7a05ec4357b31f753b4de25883dd504
Stephen Mose Aaskov <stephen@aaskov.com>
2021-12-26 02:06:20 +0100

    Made build of firmware work on M1

--- radio/src/storage/rtc_backup.cpp
+++ radio/src/storage/rtc_backup.cpp
@@ -19,6 +19,7 @@
  * GNU General Public License for more details.
  */
 
+
 #include "opentx.h"
 #include "rtc_backup.h"
 
--- radio/src/storage/sdcard_raw.cpp
+++ radio/src/storage/sdcard_raw.cpp
@@ -19,6 +19,7 @@
  * GNU General Public License for more details.
  */
 
+
 #include "opentx.h"
 #include "modelslist.h"
 #include "conversions/conversions.h"
